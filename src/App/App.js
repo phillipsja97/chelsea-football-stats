d16661dcc650d2287736fa78183f09b98a1cb50f
unknown <phillipsja97@gmail.com>
2022-10-07 11:42:11 -0500

    added navbar

--- src/App/App.js
+++ src/App/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Navbar from '../Components/NavBar/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <p>Hello World</p>
     </div>
   );
